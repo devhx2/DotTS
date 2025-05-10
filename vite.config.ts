986e8665217d9346e49efcb2f6b4006bef7f916e
devhx2 <devhx2@gmail.com>
2025-05-10 21:56:55 +0900

    fix: fix import path alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@dotts": path.resolve(__dirname, "/src/index.ts"),
+      "@dotts": path.resolve(__dirname, "src/index.ts"),
     },
   },
   server: {
