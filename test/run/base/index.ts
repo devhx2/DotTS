4659386ecb850ca7cec2e19c3b69aeeeeec65333
devhx2 <devhx2@gmail.com>
2025-05-10 18:33:13 +0900

    update: implement sprite movement and rendering using logical units

--- test/run/base/index.ts
+++ test/run/base/index.ts
@@ -14,6 +14,7 @@ type Config = {
     height: number;
     scale: number;
   };
+  fps: number;
 };
 
 const config: Config = {
@@ -23,6 +24,7 @@ const config: Config = {
     height: 480,
     scale: 4,
   },
+  fps: 30,
 };
 
 main(config);
@@ -32,11 +34,8 @@ function main(config: Config) {
   const ctx = canvas.getContext("webgl") as WebGLRenderingContext;
   if (!ctx) alert("Not Supported WebGL!");
 
-  canvas.width = config.canvas.width / config.canvas.scale;
-  canvas.height = config.canvas.height / config.canvas.scale;
-  canvas.style.width = `${config.canvas.width}px`;
-  canvas.style.height = `${config.canvas.height}px`;
-  canvas.style.imageRendering = "pixelated";
+  canvas.width = config.canvas.width;
+  canvas.height = config.canvas.height;
 
   ctx.viewport(0, 0, canvas.width, canvas.height);
   ctx.enable(ctx.BLEND);
@@ -60,6 +59,7 @@ function main(config: Config) {
 
     const aPosition = ctx.getAttribLocation(program, "aPosition");
     const aTexCoord = ctx.getAttribLocation(program, "aTexCoord");
+    const uScale = ctx.getUniformLocation(program, "uScale");
     const uOffset = ctx.getUniformLocation(program, "uOffset");
     const uCanvasSize = ctx.getUniformLocation(program, "uCanvasSize");
 
@@ -95,28 +95,27 @@ function main(config: Config) {
       ctx.bindBuffer(ctx.ARRAY_BUFFER, buffer);
       ctx.bufferData(ctx.ARRAY_BUFFER, vertices, ctx.STATIC_DRAW);
 
-      const baseSpeed =
-        Math.round(canvas.width / 30 / config.canvas.scale) *
-        config.canvas.scale;
+      const width = canvas.width / config.canvas.scale;
+      const speed = Math.round(width / (config.fps * 8));
+
       const sprite = {
-        x: Math.random() * (canvas.width - w),
-        y: Math.random() * (canvas.height - h),
-        speedX: (Math.random() < 0.5 ? -1 : 1) * baseSpeed,
-        speedY: (Math.random() < 0.5 ? -1 : 1) * baseSpeed,
+        x: 0,
+        y: 0,
+        speedX: speed,
+        speedY: speed,
         texture: loadTexture(ctx, `../texture/sample_16x16.png`),
         buffer: buffer,
       };
       sprites.push(sprite);
     }
 
-    const fps = 30;
-    const interval = 1000 / fps;
+    const interval = 1000 / config.fps;
     let lastTime = 0;
 
     function loop(currentTime: number) {
       if (currentTime - lastTime >= interval) {
         update(canvas, sprites);
-        render(ctx, aPosition, aTexCoord, uOffset!, sprites);
+        render(ctx, aPosition, aTexCoord, uOffset!, uScale!, sprites);
         lastTime = currentTime;
       }
       requestAnimationFrame(loop);
@@ -153,7 +152,6 @@ function loadTexture(gl: WebGLRenderingContext, src: string) {
   image.src = src;
   image.onload = () => {
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
@@ -168,36 +166,40 @@ function update(canvas: HTMLCanvasElement, sprites: Sprite[]) {
     sprite.x += sprite.speedX;
     sprite.y += sprite.speedY;
 
-    const scale = config.canvas.scale;
-    sprite.x = Math.round(sprite.x / scale) * scale;
-    sprite.y = Math.round(sprite.y / scale) * scale;
+    sprite.x = Math.round(sprite.x);
+    sprite.y = Math.round(sprite.y);
 
-    if (sprite.x < 0 || sprite.x > canvas.width - 16) sprite.speedX *= -1;
-    if (sprite.y < 0 || sprite.y > canvas.height - 16) sprite.speedY *= -1;
+    if (sprite.x < 0 || sprite.x > canvas.width / 4 - 16) sprite.speedX *= -1;
+    if (sprite.y < 0 || sprite.y > canvas.height / 4 - 16) sprite.speedY *= -1;
   }
 }
 
 function render(
-  gl: WebGLRenderingContext,
+  ctx: WebGLRenderingContext,
   aPosition: number,
   aTexCoord: number,
   uOffset: WebGLUniformLocation,
+  uScale: WebGLUniformLocation,
   sprites: Sprite[]
 ) {
-  gl.clearColor(51 / 255, 187 / 255, 51 / 255, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  ctx.clearColor(51 / 255, 187 / 255, 51 / 255, 1.0);
+  ctx.clear(ctx.COLOR_BUFFER_BIT);
 
   for (const sprite of sprites) {
-    gl.bindTexture(gl.TEXTURE_2D, sprite.texture);
-    gl.bindBuffer(gl.ARRAY_BUFFER, sprite.buffer);
+    ctx.bindTexture(ctx.TEXTURE_2D, sprite.texture);
+    ctx.bindBuffer(ctx.ARRAY_BUFFER, sprite.buffer);
 
-    gl.enableVertexAttribArray(aPosition);
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 16, 0);
+    ctx.enableVertexAttribArray(aPosition);
+    ctx.vertexAttribPointer(aPosition, 2, ctx.FLOAT, false, 16, 0);
 
-    gl.enableVertexAttribArray(aTexCoord);
-    gl.vertexAttribPointer(aTexCoord, 2, gl.FLOAT, false, 16, 8);
+    ctx.enableVertexAttribArray(aTexCoord);
+    ctx.vertexAttribPointer(aTexCoord, 2, ctx.FLOAT, false, 16, 8);
 
-    gl.uniform2f(uOffset, sprite.x, sprite.y);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    const x = Math.round(sprite.x);
+    const y = Math.round(sprite.y);
+
+    ctx.uniform2f(uOffset, x, y);
+    ctx.uniform1f(uScale, config.canvas.scale);
+    ctx.drawArrays(ctx.TRIANGLE_STRIP, 0, 4);
   }
 }
--- test/run/shader/sprite.vert
+++ test/run/shader/sprite.vert
@@ -1,12 +1,18 @@
 attribute vec2 aPosition;
 attribute vec2 aTexCoord;
 varying vec2 vTexCoord;
+
+uniform float uScale;
 uniform vec2 uOffset;
 uniform vec2 uCanvasSize;
 
 void main() {
-  vec2 pos = aPosition + uOffset;
-  vec2 clipPos = (pos / uCanvasSize) * 2.0 - 1.0;
-  gl_Position = vec4(clipPos, 0.0, 1.0);
+  vec2 pos = (aPosition + uOffset) * uScale;
+
+  vec2 zeroToOne = pos / uCanvasSize;
+  vec2 zeroToTwo = zeroToOne * 2.0;
+  vec2 clipSpace = zeroToTwo - 1.0;
+
+  gl_Position = vec4(clipSpace.x, -clipSpace.y, 0, 1);
   vTexCoord = aTexCoord;
 }
